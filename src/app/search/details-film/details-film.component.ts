bc304fbe5c5e82189a682476da7e06cc7e51cbea
Majdi Rabie <majdi.rabie@esprit.tn>
2019-11-15 17:40:52 +0100

    fix interface Film and details_Component

--- src/app/search/details-film/details-film.component.ts
+++ src/app/search/details-film/details-film.component.ts
@@ -11,6 +11,10 @@ import {Film} from '../../core/models/film';
 export class DetailsFilmComponent implements OnInit {
   filmId: string;
   film: any = {} as Film;
+  rating: number = 0;
+  x_rating: number = 10;
+  z_rating = new Array();
+  y_rating = new Array();
 
   constructor(private filmService: FilmsService,
               private route: ActivatedRoute,
@@ -19,16 +23,22 @@ export class DetailsFilmComponent implements OnInit {
 
   ngOnInit() {
     this.filmId = this.route.snapshot.paramMap.get('imdbID');
+
     this.getFilmByimdbID();
   }
 
   getFilmByimdbID() {
     this.filmService.getFilm(this.filmId, '295e6386').subscribe(data => {
-      this.film=data
+      this.film = data;
+      this.rating = Math.round(+this.film.imdbRating);
+      this.x_rating = this.x_rating - this.rating;
+      this.z_rating=new Array(this.rating)
+      this.y_rating=new Array(this.x_rating)
+      console.log(this.rating);
     });
   }
 
-  goToListFilm(){
+  goToListFilm() {
     this.router.navigate(['/']);
   }
 }
